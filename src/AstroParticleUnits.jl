47c763242b4af966db950c0e16d9957fb0969ac1
kcarloni <kcarloni@g.harvard.edu>
2025-06-25 18:45:24 -0400

    organization + compact show, for typedtables

--- src/AstroParticleUnits.jl
+++ src/AstroParticleUnits.jl
@@ -17,159 +17,182 @@ export @u_str, unit, Quantity, ustrip, NoDims, uparse, FreeUnits
 export natural, unnatural, uconvert
 export prefix, abbr, power
 
-# import units: 
-unitful_to_import = (
-    :m, :s, :eV,
-    :c0, :q, :ħ,
-    :yr,
-    :b,
-    :Gauss, 
-    :rad, :°, :sr 
-)
-for u in unitful_to_import
-    @eval import Unitful: $u
-end
-import UnitfulAstro: AU, ly, pc, Msun, Rsun
-import NaturallyUnitful: c, ħ
-import PhysicalConstants: CODATA2022.α
+# ===================
+# setup units
 
-units_to_export = [
-    :c, :ħ, :α,
-    :m, :s, :eV,
-    :rad, :°, :sr,
-    :c0, 
-    #:q, 
-    # :ly, 
-    :AU, :pc, 
-    :Msun, :Rsun
-]
+    # import units: 
+    unitful_to_import = (
+        :m, :s, :eV,
+        :c0, :q, :ħ,
+        :yr,
+        :b,
+        :Gauss, 
+        :rad, :°, :sr 
+    )
+    for u in unitful_to_import
+        @eval import Unitful: $u
+    end
+    import UnitfulAstro: AU, ly, pc, Msun, Rsun
+    import NaturallyUnitful: c, ħ
+    import PhysicalConstants: CODATA2022.α
 
-# additionally useful constants:
-const cm = u"cm"
-const km = u"km"
-append!( units_to_export, [:cm, :km] )
+    units_to_export = [
+        :c, :ħ, :α,
+        :m, :s, :eV,
+        :rad, :°, :sr,
+        :c0, 
+        #:q, 
+        # :ly, 
+        :AU, :pc, 
+        :Msun, :Rsun
+    ]
 
-const MeV = u"MeV"
-const GeV = u"GeV"
-const TeV = u"TeV"
-const PeV = u"PeV"
-const EeV = u"EeV"
-append!( units_to_export, [:MeV, :GeV, :TeV, :PeV, :EeV] )
+    # additionally useful constants:
+    const cm = u"cm"
+    const km = u"km"
+    append!( units_to_export, [:cm, :km] )
 
-const kpc = u"kpc"
-const Mpc = u"Mpc"
-const Gpc = u"Gpc"
-append!( units_to_export, [:kpc, :Mpc, :Gpc] )
+    const MeV = u"MeV"
+    const GeV = u"GeV"
+    const TeV = u"TeV"
+    const PeV = u"PeV"
+    const EeV = u"EeV"
+    append!( units_to_export, [:MeV, :GeV, :TeV, :PeV, :EeV] )
 
-const mb = u"mb"
-# const μb = u"μb"
-const μG = u"μGauss"
-append!( units_to_export, [:mb, :μG, :Gauss] )
+    const kpc = u"kpc"
+    const Mpc = u"Mpc"
+    const Gpc = u"Gpc"
+    append!( units_to_export, [:kpc, :Mpc, :Gpc] )
 
-const q_electron = u"q"
-const m_proton = Particle("proton").mass.value * c^2
-const m_electron = Particle("electron").mass.value * c^2
-append!( units_to_export, [:m_proton, :m_electron, :q_electron] )
+    const mb = u"mb"
+    # const μb = u"μb"
+    const μG = u"μGauss"
+    append!( units_to_export, [:mb, :μG, :Gauss] )
 
-const m_pi0 = Particle("pi0").mass.value * c^2
-const m_muon = Particle("muon").mass.value * c^2 
-const m_tau = Particle("tau").mass.value * c^2
-append!( units_to_export, [:m_pi0, :m_muon] )
+    const q_electron = u"q"
+    const m_proton = Particle("proton").mass.value * c^2
+    const m_electron = Particle("electron").mass.value * c^2
+    append!( units_to_export, [:m_proton, :m_electron, :q_electron] )
 
-# const m_Wboson = Particle("W+").mass.value * c^2
-# const m_Zboson = Particle("Z0").mass.value * c^2
-# sin_sq_θW = 1 - (m_Wboson / m_Zboson)^2
+    const m_pi0 = Particle("pi0").mass.value * c^2
+    const m_muon = Particle("muon").mass.value * c^2 
+    const m_tau = Particle("tau").mass.value * c^2
+    append!( units_to_export, [:m_pi0, :m_muon] )
 
-# CODATA 2022
-const sin_sq_θW = 0.22305
-push!( units_to_export, :sin_sq_θW )
+    # const m_Wboson = Particle("W+").mass.value * c^2
+    # const m_Zboson = Particle("Z0").mass.value * c^2
+    # sin_sq_θW = 1 - (m_Wboson / m_Zboson)^2
 
-# natural units
-const G_fermi = 1.1663787e-5 * GeV^(-2)
-push!( units_to_export, :G_fermi )
+    # CODATA 2022
+    const sin_sq_θW = 0.22305
+    push!( units_to_export, :sin_sq_θW )
 
-# const ϵ0 = u"ϵ0"
-# const kg = u"kg"
-# const g_earth = u"ge"
-# const N = u"N"
-# const C = u"C"
-# export ϵ0, kg, ge, N, C
+    # natural units
+    const G_fermi = 1.1663787e-5 * GeV^(-2)
+    push!( units_to_export, :G_fermi )
 
-for x in units_to_export
-    @eval export $x
-end
+    # const ϵ0 = u"ϵ0"
+    # const kg = u"kg"
+    # const g_earth = u"ge"
+    # const N = u"N"
+    # const C = u"C"
+    # export ϵ0, kg, ge, N, C
 
-
-# convenience: unit canceling in division / multiplication
-
-Base.:/( num::Quantity{T1, D, U}, denom::Quantity{T2, D, U}) where {T1, T2, U, D} = num.val / denom.val
-Base.:/( num::Quantity{T1, D, U1}, denom::Quantity{T2, D, U2}) where {T1, T2, U1, U2, D} = uconvert(unit(denom), num) / denom
-
-# Base.:*( 
-#     num::Quantity{T1, D, U}, 
-#     denom::Quantity{T2, D, U}) where {T1, T2, U, D} = 
-#         num.val * denom.val * unit(num) * unit(denom)
-    
-# Base.:*( 
-#     num::Quantity{T1, D, U1}, 
-#     denom::Quantity{T2, D, U2}) where {T1, T2, U1, U2, D} = 
-#         uconvert(unit(denom), num) * denom
-
-
-# use UnitfulParsableString.jl instead (?)
-"""
-    get_parseable_string_for_unit(u)
-
-unlike string(u), returns a string that can be parsed by `uparse`. 
-"""
-function get_parseable_string_for_unit(u)
-
-    if typeof(u).parameters[1] == ()
-        return ""
+    for x in units_to_export
+        @eval export $x
     end
 
-    utup = [(
-        abbr = prefix(ui)*abbr(ui),
-        pow = power(ui)
-    ) for ui in typeof(u).parameters[1]]
+#
 
-    prod([
-        string(ui.abbr)*"^("*string(ui.pow)*") * " for ui in utup
-    ])[1:end-3]
-end
-export get_parseable_string_for_unit
+# ===================
+# convenience: unit canceling in division / multiplication
 
-function latexstring( u::FreeUnits )
+    Base.:/( num::Quantity{T1, D, U}, denom::Quantity{T2, D, U}) where {T1, T2, U, D} = num.val / denom.val
+    Base.:/( num::Quantity{T1, D, U1}, denom::Quantity{T2, D, U2}) where {T1, T2, U1, U2, D} = uconvert(unit(denom), num) / denom
 
-    utup = [(
-        abbr = prefix(ui)*abbr(ui),
-        pow = power(ui)
-    ) for ui in typeof(u).parameters[1]]
+    # Base.:*( 
+    #     num::Quantity{T1, D, U}, 
+    #     denom::Quantity{T2, D, U}) where {T1, T2, U, D} = 
+    #         num.val * denom.val * unit(num) * unit(denom)
+        
+    # Base.:*( 
+    #     num::Quantity{T1, D, U1}, 
+    #     denom::Quantity{T2, D, U2}) where {T1, T2, U1, U2, D} = 
+    #         uconvert(unit(denom), num) * denom
 
-    return latexstring( prod(
-        ( ui.pow == 1 ) ? L"\mathrm{%$(ui.abbr)}" : L"\mathrm{%$(ui.abbr)}^{%$( ui.pow.den == 1 ? ui.pow.num : Float64(ui.pow) )}"
-        # prod( L"\mathrm{%$(ui.abbr)}^{%$( format_pow(ui.pow) )}" 
-        for ui in utup )
-    )
-end
+#
 
+# ====================
+# unit parsing
 
+    # use UnitfulParsableString.jl instead (?)
+    """
+        get_parseable_string_for_unit(u)
 
-const AstroParticle_base_unit_context = [ Unitful, UnitfulAstro ]
+    unlike string(u), returns a string that can be parsed by `uparse`. 
+    """
+    function get_parseable_string_for_unit(u)
 
-# """
-#     uparse(string)
+        if typeof(u).parameters[1] == ()
+            return ""
+        end
 
-# Re-exported by AstroParticleUnits from Unitful with a default
-#     `unit_context = AstroParticle_base_unit_context`.
+        utup = [(
+            abbr = prefix(ui)*abbr(ui),
+            pow = power(ui)
+        ) for ui in typeof(u).parameters[1]]
 
-# Outputs of `get_parseable_string_for_unit` are parseable. 
-# """
-# uparse( str ) = uparse( str; unit_context=AstroParticle_base_unit_context )
-# __precompile__(false)
+        prod([
+            string(ui.abbr)*"^("*string(ui.pow)*") * " for ui in utup
+        ])[1:end-3]
+    end
+    export get_parseable_string_for_unit
 
-unitparse( str ) = uparse( str; unit_context=AstroParticle_base_unit_context )
-export unitparse
+    const AstroParticle_base_unit_context = [ Unitful, UnitfulAstro ]
+
+    # """
+    #     uparse(string)
+
+    # Re-exported by AstroParticleUnits from Unitful with a default
+    #     `unit_context = AstroParticle_base_unit_context`.
+
+    # Outputs of `get_parseable_string_for_unit` are parseable. 
+    # """
+    # uparse( str ) = uparse( str; unit_context=AstroParticle_base_unit_context )
+    # __precompile__(false)
+
+    unitparse( str ) = uparse( str; unit_context=AstroParticle_base_unit_context )
+    export unitparse
+
+#
+
+# =============================
+# latex + show
+
+    function latexstring( u::FreeUnits )
+
+        utup = [(
+            abbr = prefix(ui)*abbr(ui),
+            pow = power(ui)
+        ) for ui in typeof(u).parameters[1]]
+
+        return latexstring( prod(
+            ( ui.pow == 1 ) ? L"\mathrm{%$(ui.abbr)}" : L"\mathrm{%$(ui.abbr)}^{%$( ui.pow.den == 1 ? ui.pow.num : Float64(ui.pow) )}"
+            # prod( L"\mathrm{%$(ui.abbr)}^{%$( format_pow(ui.pow) )}" 
+            for ui in utup )
+        )
+    end
+
+    function Base.show(io::IO, v::AbstractVector{T}) where {T <: Quantity}
+        compact = get(io, :compact, false)
+
+        if !compact
+            show(io, v)
+        end
+            print(io, "[" * prod( string.(x) .* ", " )[1:end-2] * "]" )
+    end
+
+#
 
 macro uconvert(u, x)
     return quote
